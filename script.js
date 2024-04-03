de0eca3ee3708ae3f8f76031933263aea50858dd
Matteo Daniele <matteodaniele222@gmail.com>
2024-04-03 17:24:41 -0300

    Change the function to upload images in popup, it isn't finish, it has a mistake in a div

--- script.js
+++ script.js
@@ -96,36 +96,128 @@ options.forEach(option => {
 menu.addEventListener('click', (e) => {
     e.stopPropagation();
 })
-var galleryImage = document.getElementById("galleryImage");
-let divContainerRightImages = document.createElement("div"); 
-let divContainerLeftImages = document.createElement("div");
-var loadFile= function(event){
-    for(let i=0; i<event.target.files.length; i++){
-        console.log(i);
-        let imageGallery = document.createElement("img");
-        imageGallery.src = URL.createObjectURL(event.target.files[i]);
 
-        if(i === 1 || i === 2){
-            divContainerLeftImages.appendChild(imageGallery);
+// Images Popup
+const imagesArray = [];
+var galleryImage = document.getElementById("galleryImage");
+let divContainerRightImages = document.createElement("div");
+let divContainerLeftImages = document.createElement("div");
+let divContainerLeftImagesTop = document.createElement("div");
+let divContainerLeftImagesBot = document.createElement("div");
+let divContainerRightImagesTop = document.createElement("div");
+let divContainerRightImagesBot = document.createElement("div");
+function loadFile(event){
+    
+        let eventTarget = event.target;
+        let files = eventTarget.files;
+        for(let i = 0; i < files.length; i++){
+            let file = files[i];
+            let url = URL.createObjectURL(file);
+            imagesArray.push(url);
         }
-        if(i === 0 || i === 3){
-            divContainerRightImages.appendChild(imageGallery);
+        console.log("La dimension del array es " + imagesArray.length);
+        // console.log(i);
+        // let imageGallery = document.createElement("img");
+        // imageGallery.src = URL.createObjectURL(event.target.files[i]);
+        // imageGallery.classList.add("images-gallery");
+        // let removeImage = document.createElement("span"); 
+        // removeImage.innerHTML = "\u00d7";
+        // removeImage.classList.add("close");
+        // removeImage.classList.add("position-close-image");
+        //  if(i === 1 || i === 2){
+        //      divContainerLeftImages.appendChild(imageGallery);
+        //  }
+        //  if(i === 0 || i === 3){
+        //      divContainerRightImages.appendChild(imageGallery);
+        // }    console.log(imagesArray);
+    // for(let i=0; i<imagesArray.length - 1; i++){
+    //     let imageGallery = document.createElement("img");
+    //     imageGallery.classList.add("images-gallery");
+    //     if(i === 0 && divContainerRightImagesTop.childElementCount === 0){
+    //         imageGallery.src = imagesArray[i];
+    //         divContainerRightImagesTop.appendChild(imageGallery);
+    //         divContainerRightImages.appendChild(divContainerRightImagesTop);
+    //     }else if(i % 2 === 0 && divContainerRightImagesTop.childElementCount === 1){
+    //         imageGallery.src = imagesArray[i];
+    //         divContainerRightImagesBot.appendChild(imageGallery);
+    //         divContainerRightImages.appendChild(divContainerRightImagesBot);
+    //     }
+    //     if(i % 2 === 1 && divContainerLeftImagesTop.childElementCount === 0){
+    //         imageGallery.src = imagesArray[i];
+    //         divContainerLeftImagesTop.appendChild(imageGallery);
+    //         divContainerLeftImages.appendChild(divContainerLeftImagesTop);
+    //     }else if(i % 2 === 1 && divContainerLeftImagesTop.childElementCount === 1){
+    //         imageGallery.src = imagesArray[i];
+    //         divContainerLeftImagesBot.appendChild(imageGallery);
+    //         divContainerLeftImages.appendChild(divContainerLeftImagesBot);
+    //     }
+    // }
+    // if(divContainerRightImages.childElementCount === 2 && divContainerLeftImages.childElementCount === 0){
+
+    //     let secondChild = divContainerRightImages.children[1];
+    //     divContainerLeftImages.appendChild(secondChild);
+
+    // }else if(divContainerRightImages.childElementCount === 2 && divContainerLeftImages.childElementCount === 1){
+    //     let firstChildRight = divContainerRightImages.children[0];
+    //     let secondChildRight = divContainerRightImages.children[1];
+
+    //     divContainerRightImagesTop.appendChild(firstChildRight);
+    //     divContainerRightImagesBot.appendChild(secondChildRight);
+    
+    //     divContainerRightImages.appendChild(divContainerRightImagesTop);
+    //     divContainerRightImages.appendChild(divContainerRightImagesBot);
+    
+    // }else if(divContainerRightImages.childElementCount === 3 && divContainerLeftImages.childElementCount === 1){
+    //     let lastChildRight = divContainerRightImages.children[2];
+    //     let firstChildLeft = divContainerLeftImages.children[0];
+
+    //     divContainerLeftImagesTop.appendChild(firstChildLeft);
+    //     divContainerLeftImagesBot.appendChild(lastChildRight);
+
+    //     divContainerLeftImages.appendChild(divContainerLeftImagesTop);
+    //     divContainerLeftImages.appendChild(divContainerLeftImagesBot);
+    // }
+    // if(divContainerRightImages.childElementCount > 0){
+    //     divContainerRightImages.classList.add("images-popup-right");
+    // }
+    // if(divContainerLeftImages.childElementCount > 0){
+    //     divContainerLeftImages.classList.add("images-popup-left");
+    //    divContainerRightImages.classList.add("images-popup-right-width");
+    // }
+}
+function renderImages(){
+    console.log("Tu wacha entra");
+    let removeImage = document.createElement("span"); 
+    removeImage.innerHTML = "\u00d7";
+    removeImage.classList.add("close");
+    removeImage.classList.add("position-close-image");
+    for(let i = 0; i < imagesArray.length; i++){
+        let url = imagesArray[i];
+        let imageGallery = document.createElement("img");
+        imageGallery.classList.add("images-gallery");
+        imageGallery.src = url;
+        if(i === 0 && divContainerRightImagesTop.childElementCount === 0){
+            divContainerRightImagesTop.appendChild(imageGallery);
+            divContainerRightImages.appendChild(divContainerRightImagesTop);
+            galleryImage.appendChild(divContainerRightImages);
+            divContainerRightImages.classList.add("images-popup-right");
+        }else if(i === 1 && divContainerLeftImagesTop.childElementCount === 0){
+            divContainerLeftImagesTop.appendChild(imageGallery);
+            divContainerLeftImages.appendChild(divContainerLeftImagesTop);
+            galleryImage.appendChild(divContainerLeftImages);
+            divContainerLeftImages.classList.add("images-popup-left");
+            divContainerRightImages.classList.add("images-popup-right-width");
+        }else if(i === 2 && divContainerRightImagesBot.childElementCount === 0){
+            divContainerRightImagesBot.appendChild(imageGallery);
+            divContainerRightImages.appendChild(divContainerRightImagesBot);
+        }else if(i === 3 && divContainerLeftImagesBot.childElementCount === 0){
+            divContainerLeftImagesBot.appendChild(imageGallery);
+            divContainerLeftImages.appendChild(divContainerLeftImagesBot);
         }
-    }
-    if(divContainerRightImages.childElementCount === 2 && divContainerLeftImages.childElementCount === 0){
-        let secondChild = divContainerRightImages.children[1];
-        divContainerLeftImages.appendChild(secondChild);
-    }else if(divContainerRightImages.childElementCount === 3 && divContainerLeftImages.childElementCount === 1){
-        let lastChild = divContainerRightImages.children[2];
-        divContainerLeftImages.appendChild(lastChild);
-    }
-    if(divContainerRightImages.childElementCount > 0){
-        divContainerRightImages.classList.add("images-popup-right");
-    }
-    if(divContainerLeftImages.childElementCount > 0){
-        divContainerLeftImages.classList.add("images-popup-left");
-       divContainerRightImages.classList.add("images-popup-right-width");
-    }
-    galleryImage.appendChild(divContainerLeftImages);
-    galleryImage.appendChild(divContainerRightImages);
+    }  
+}
+function saveAndRenderImages(event){
+    console.log("1");
+    loadFile(event);
+    renderImages();
 }
